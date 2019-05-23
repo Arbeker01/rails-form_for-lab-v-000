aa589804252b59cd2f64cec0d276a429f9834953
Romeo Gono <romeogono1@aol.com>
2019-05-23 04:00:50 +0000

    Automatically backed up by Learn

--- spec/helpers/school_classes_controller_helper_spec.rb
+++ spec/helpers/school_classes_controller_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe SchoolClassesControllerHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#RSpec.describe SchoolClassesControllerHelper, type: :helper do
+  #pending "add some examples to (or delete) #{__FILE__}"
+#end
